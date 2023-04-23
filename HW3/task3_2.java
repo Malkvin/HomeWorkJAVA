568c10c6638e96bcbdd9dbaf65ba2881b2b43e7f
Artem <artemmarco@gmail.com>
2023-04-23 15:44:06 +0300

    Домашнее задание к семинару 3

---
+++ HW3/task3_2.java
@@ -0,0 +1,23 @@
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка. Collections.max()
+package HW3;
+import java.util.*;
+public class task3_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(generateRandomInt(20));
+        }
+        double average = 0;
+        for (int i = 0; i < list.size(); i++) {
+            average += list.get(i);
+        }
+    System.out.println(list);
+    System.out.println("Минимальный элемент: " + Collections.min(list));
+    System.out.println("Максимальный элемент: " + Collections.max(list));
+    System.out.println("Среднее значение: " + average / list.size());
+    }
+    public static int generateRandomInt(int upperRange){
+        Random random = new Random();
+        return random.nextInt(upperRange);
+    }     
+}
