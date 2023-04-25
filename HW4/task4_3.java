41e2e9dd0e1cfcfb106299b60ba3654eb51683dd
Artem <artemmarco@gmail.com>
2023-04-25 15:16:46 +0300

    Домашнее задание к семинару 4

---
+++ HW4/task4_3.java
@@ -0,0 +1,33 @@
+package HW4;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Random;
+public class task4_3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> MyLinkedList = getRandomNumber(10, 5);
+        int sum = getSumElemList(MyLinkedList);
+        System.out.print("Cозданный LinkedList: ");
+        System.out.println(MyLinkedList);
+        System.out.printf("Cумма : %d\n", sum);
+    }
+
+    static LinkedList<Integer> getRandomNumber(int size, int upperBond) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Random ri = new Random();
+        for (int i = 0; i < size; i++) {
+            linkedList.add(ri.nextInt(upperBond));
+        }
+        return linkedList;
+    }
+
+    static int getSumElemList(LinkedList<Integer> linkedList) {
+        Iterator<Integer> iteratorList = linkedList.iterator();
+        int total = 0;
+        while(iteratorList.hasNext()) {
+            int item = iteratorList.next();
+            total+=item;
+        }
+        return total;
+    }
+}
+
