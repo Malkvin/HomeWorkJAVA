cd1dfa0a1e05ecafca56d8c585fa506d37212d2a
Artem <artemmarco@gmail.com>
2023-05-19 11:17:59 +0300

    Итоговый проект домашнее задание 6

---
+++ HW6/NoteBook.java
@@ -0,0 +1,30 @@
+package HW6;
+public class NoteBook {
+    private String brand;
+    private String model;
+    private String OS;
+    private int ram;
+    private int hdSize;
+    public NoteBook(String brand, String model, String OS, int ram, int hdSize) {
+        this.brand = brand;
+        this.model = model;
+        this.OS = OS;
+        this.ram = ram;
+        this.hdSize = hdSize;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getOS() {
+        return OS;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public int getHdSize() {
+        return hdSize;
+    }
+}
